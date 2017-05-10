2c4a824fc11ae5e70f48a0a588acdfc1dd12101b
LiHao <haoli@rtigertech.com>
2017-05-10 17:13:56 +0800

    init

---
+++ EBikeApp/app/src/main/java/com/trust/ebikeapp/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.trust.ebikeapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Trust on 2017/5/10.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
